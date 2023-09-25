e4eac076d71b969ab76e5537e8322e8beaa1e4b2
samueldonga <146006557+samueldonga@users.noreply.github.com>
2023-09-25 18:50:44 +0300

    Add files via upload

---
+++ Services.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+
+<style>
+
+
+body
+{
+background-color:rgb(191, 245, 191);
+}
+
+.menu_bar
+{
+
+top: 80%;
+width: 100%;
+height: 8%;
+background-color: lightblue;
+color: white;
+justify-content: center;
+align-items: space-between;
+border-radius: 40px;
+}
+
+button
+{
+border: dotted 1px yellow;
+top: 5px;
+position: relative;
+background: white;
+border-radius: 40px;
+cursor: pointer;
+}
+
+button:hover
+{
+background:orange;
+width:120px;
+color: white;
+border-radius:50px;
+}
+
+
+hr
+{
+background-color: black;
+border-radius: 30px;
+height: 3px;
+}
+
+.footer
+{
+color: blue;
+}
+
+</style>
+
+<title>About us page</title>
+<script type="text/javascript">
+function message()
+{
+	alert("THANKS FOR TRUSTING BUDAKA-MBALE ROAD POLICE STATION BECAUSE 'YOUR LIVES ARE OUR RESPONSIBILITY'");
+}
+	</script>
+</head>
+
+<body onload="message()">
+<center>
+<h1><br>BUDAKA-MBALE ROAD POLICE STATION<br><font size ="05">ACCIDENTS REPORTING SYSTEM</font></h1><br>
+<img src ="im17.jpg" width="20%" height="20%" style="border-radius:100%";>
+<div class="menu_bar">
+<a href="Home.php"><button>Home page</button></a>
+<a href="Services.php"><button>About us</button></a>
+<a href="display.php"><button>Reported accidents</button></a>
+<a href="index.php"><button>Reporting Form</button></a>
+</div>
+</center>
+<center>
+<img src ="im19.jpg" height ="20%" width ="20%"></center>
+<br>
+<center><h1><b><font color="red">Background</font></h1></center>
+<font color="black">
+<p><center>Budaka-Mbale Road police accidents reporting system was founded in 2000.It was abush before and this made us to thinck of what system to do and we thougt of doing the police reporting system to improve on the security along Budaka-Mbale road.<br>
+And this happend when when one of the Board members introduced the idea after discovering the high rates of road accidents in uganda and african at large.A big number of poeple we loosing lives and atfer this
+one beloved ones' life was  in denger .<br>
+<br>
+However other activities like thefty is always involved. People around the scene take the properties of the victims before the police arrives</p></center>
+<p><center><img src ="mm.JPG" height ="20%" width ="13%" style="border-radius:100%;"</center></p>Therefore Mr: DONGA SAMUEL the Chair person local goverment(IV) of BUDAKA-MBALE ROAD POLICE STATION by then came up with an idea 
+of coming up with a Road Accidents Reporting system<br> hoping for the solutions to various problems faced the poeple
+after accidents happening in the area.<br> 
+<br>In this, the reporter can also give description of how the accident was caused<br>
+and penalties can be imposed on the causers who tend not to abide by the rules of traffic.</p>
+
+<h2><font color="red">Mission</font></h2>
+<p>"To provide protection services to people involved in accidents and save lives of other injured"</p>
+<h1><font color="red">Vision</font></h1>
+<p>"To quickly save people's properties and lives"</p>
+<h1><font color="red">Services</font></h1>
+<p>Follow us on the causes of accident by logining in the system.</p>
+<p>Providing protection to the affected people and sendind the dead to they respective homes
+and arest those who tend not abide by the laws by taking them to courts of law<br>
+Alerting government about the poor infrastructures and calling for its help where need be<p>
+</font>
+<h1><font color="red">You can follow us on;</font></h1><b>
+
+<p><img src ="w.jpg" height ="07%" width ="05%" style="border-radius:100%";> <a href="https://bmrps.whatspp.com.ug"><button>bmrps.whatspp.com.ug</button></a><img src ="ti.jpg" height ="07%" width ="05%" style="border-radius:100%";> +256-789047767/+256-759642660
+ <img src ="twitter.png" height ="07%" width ="05%" style="border-radius:100%";> <a href="https://bmrps.twitter.com.ug"><button>bmrps.twitter.com.ug</button></a>  
+<img src ="u.jpg" height ="07%" width ="05%" style="border-radius:100%";> <a href="https://bmrps.youtube.com.ug"><button>bmrps.youtube.com.ug</button></a></p> 
+<p><img src ="envelope.png" height ="07%" width ="05%" style="border-radius:100%";>  <a href="https://bmrps@gmail.com.ug"><button>bmrps@gmail.com.ug</button></a> 
+<img src ="fb.png" height ="07%" width ="05%" style="border-radius:100%";> <a href="https://bmrps.facebook.com.ug"><button>bmrps.facebook.com.ug</button></a> 
+<img src ="fh.jpg" height ="07%" width ="05%" style="border-radius:100%";> <a href="https://bmrps.skype.com.ug"><button>bmrps.skype.com.ug</button></a>
+<img src ="s.jpg" height ="07%" width ="05%" style="border-radius:100%";> <a href="https://bmrps.instagram.com.ug"><button>bmrps.instagram.com.ug</button></a></p> 
+<br>
+<p><img src ="marker.png" height ="07%" width ="05%"><font color="black">Budaka village. Budaka subcounty.Budaka District.Ug</font></p>
+<br>
+<br>
+<center>
+<div class="footer">
+<p><p><p><footer style="color:blue"><center>2022<sup>c</sup>Budaka District|All Rights Reserved<br>prvacy policy|Terms of use|legal information</center></footer></p>
+<hr color ="blue"></p>
+</div>
+<hr>
+</center>
+</body>
+</html>
---
+++ validate.js
@@ -0,0 +1,10 @@
+function alex()
+{
+let a = document.forms["god"]["Area"].value;
+if(a=="")
+{
+alert("location required");
+return false;
+}
+
+}
